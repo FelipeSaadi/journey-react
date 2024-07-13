0acbaba30a26ed0b0f4b7a2d7b4abd321b5f964e
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-07-13 15:12:23 -0300

    feat: add integration

--- src/pages/create-trip/confirm-trip-modal.tsx
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -1,16 +1,28 @@
 import { X, User, AtSign } from "lucide-react"
 import { FormEvent } from "react"
 import { Button } from "../../components/button"
+import { DateRange } from "react-day-picker"
+import { format } from "date-fns"
 
 interface ConfirmTripModalProps {
   closeConfirmTripModal: () => void
   createTrip: (event: FormEvent<HTMLFormElement>) => void
+  destination: string
+  eventStartAndEndDates: DateRange | undefined
+  setOwnerName: (name: string) => void
+  setOwnerEmail: (email: string) => void
 }
 
 export const ConfirmTripModal = ({
   closeConfirmTripModal,
-  createTrip
+  createTrip,
+  destination,
+  eventStartAndEndDates,
+  setOwnerName,
+  setOwnerEmail
 }: ConfirmTripModalProps) => {
+  const displayedDate = eventStartAndEndDates && eventStartAndEndDates.from && eventStartAndEndDates.to ? format(eventStartAndEndDates.from, "d' de 'LLL").concat(" até ").concat(format(eventStartAndEndDates.to, "d' de 'LLL")) : null
+
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
       <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
@@ -22,19 +34,33 @@ export const ConfirmTripModal = ({
             </button>
           </div>
           <p className="text-sm text-zinc-400">
-            Para concluir a criação da viagem <span className="text-zinc-100 font-semibold">para Florianópolis, Brasil</span> nas datas de <span className="text-zinc-100 font-semibold">16 a 27 de Agosto de 2024</span> preencha seus dados abaixo:
+            Para concluir a criação da viagem <span className="text-zinc-100 font-semibold">para {destination}</span> nas datas de <span className="text-zinc-100 font-semibold">
+              {displayedDate ?? 'não definida'}
+            </span> preencha seus dados abaixo:
           </p>
         </div>
 
         <form className="space-y-3" onSubmit={createTrip}>
           <div className="px-4 h-14 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
             <User className="text-zinc-400 size-5" />
-            <input type="text" placeholder="Seu nome completo" name="name" className="bg-transparent text-lg placeholder-zinc-400 outline-none" />
+            <input
+              type="text"
+              placeholder="Seu nome completo"
+              name="name"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none"
+              onChange={event => setOwnerName(event.target.value)}
+            />
           </div>
 
           <div className="px-4 h-14 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
             <AtSign className="text-zinc-400 size-5" />
-            <input type="email" placeholder="Seu e-mail pessoal" name="email" className="bg-transparent text-lg placeholder-zinc-400 outline-none" />
+            <input
+              type="email"
+              placeholder="Seu e-mail pessoal"
+              name="email"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none"
+              onChange={event => setOwnerEmail(event.target.value)}
+            />
           </div>
 
           <Button type="submit" variant="primary" size="full">
